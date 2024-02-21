2adb634d47ae0ed331127ecf1c7e98a72b18be65
SmolyakSA <sasmolyak@mail.ru>
2024-02-21 22:58:49 +1100

    Часть 2. Объявление переменных

--- src/main/java/ru/netology/smolyak/Main.java
+++ src/main/java/ru/netology/smolyak/Main.java
@@ -1,11 +1,31 @@
 package ru.netology.smolyak;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Sergey");
-    }
+        System.out.print("Sergey");}
+
+        public static int OperNumber;
+        // Номер операции
+        public static int UserId;
+        // Номер пользователя
+        public static float MoneyAmount;
+        // сумма по операции
+        public static short Region; // регион деятельности клиента
+
+        String OperName; // Наименование операции
+
+        public static char countryCode; // код страны оерации
+        String Inn; // Инн пользователя
+
+        public static byte operVector; // индикатор знака операции (приход-расход - ноль или единица).
+
+        String billingNumber; // номер счета
+
+        public static byte operSucsess; // индикатор успешности операции (ноль или единица).
+
 }
