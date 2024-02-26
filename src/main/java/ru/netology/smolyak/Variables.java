c258967fda98a39b3a17faa07bb36682b57059df
SmolyakSA <sasmolyak@mail.ru>
2024-02-26 22:13:29 +1100

    Часть 2. Объявление переменных переделка

--- src/main/java/ru/netology/smolyak/Variables.java
+++ src/main/java/ru/netology/smolyak/Variables.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class Variables {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var OperNumber = scanner.nextInt();
+        // Номер операции
+        int UserId = scanner.nextInt();
+        // Номер пользователя
+        float MoneyAmount = scanner.nextFloat();
+        // сумма по операции
+        short Region = scanner.nextShort(); // регион деятельности клиента
 
-    Scanner scanner = new Scanner(System.in);
-    public int OperNumber = scanner.nextInt();
-    // Номер операции
-    public static int UserId;
-    // Номер пользователя
-    public static float MoneyAmount;
-    // сумма по операции
-    public static short Region; // регион деятельности клиента
+        String OperName = scanner.nextLine(); // Наименование операции
 
-    String OperName; // Наименование операции
+        char countryCode; // код страны операции
+        String Inn; // Инн пользователя
 
-    public static char countryCode; // код страны оерации
-    String Inn; // Инн пользователя
+        boolean operVector; // индикатор знака операции (приход-расход - ноль или единица).
 
-    public static boolean operVector; // индикатор знака операции (приход-расход - ноль или единица).
+        String billingNumber; // номер счета
 
-    String billingNumber; // номер счета
-
-    public static boolean operSucsess; // индикатор успешности операции (ноль или единица).
+        boolean operSucsess; // индикатор успешности операции (ноль или единица).
 
 
-    Date date = new java.util.Date(); // дата транзакции
-
+        Date date = new Date(); // дата транзакции
+    }
 }
